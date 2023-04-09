7fac2252a6e89ddd61042b395ce1e0ad1074403c
vadniks <whatever@fake.some>
2023-04-10 00:18:01 +0300

    Structure & logic

--- src/logic.c
+++ src/logic.c
@@ -1,5 +1,6 @@
 
 #include "logic.h"
+#include "render.h"
 
 bool* gIsRunning = NULL;
 unsigned* gRendererFieldItems = NULL;
@@ -9,13 +10,36 @@ void logicInit(bool* gameRunningFlag, unsigned* rendererFieldItems) {
     gRendererFieldItems = rendererFieldItems;
 }
 
+void processKeyboardButtonPress(SDL_Keycode keycode) {
+//    SDL_Log("%s\n", SDL_GetKeyName(keycode));
+    switch (keycode) {
+        case SDLK_w:
+            SDL_Log("w\n"); // TODO: test only
+            for (unsigned i = 0; i < ROWS * COLUMNS; gRendererFieldItems[i++] = 1);
+            break;
+        case SDLK_a:
+            SDL_Log("a\n");
+            for (unsigned i = 0; i < ROWS * COLUMNS; gRendererFieldItems[i++] = 2);
+            break;
+        case SDLK_s:
+            SDL_Log("s\n");
+            for (unsigned i = 0; i < ROWS * COLUMNS; gRendererFieldItems[i++] = 3);
+            break;
+        case SDLK_d:
+            SDL_Log("d\n");
+            for (unsigned i = 0; i < ROWS * COLUMNS; gRendererFieldItems[i++] = 4);
+            break;
+        default: break;
+    }
+}
+
 void logicHandleEvent(SDL_Event* event) {
     switch (event->type) {
         case SDL_QUIT:
             *gIsRunning = false;
             break;
         case SDL_KEYDOWN:
-            SDL_Log("%s\n", SDL_GetKeyName(event->key.keysym.sym));
+            processKeyboardButtonPress(event->key.keysym.sym);
             break;
         default: break;
     }
