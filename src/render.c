e718346a6eaf386fe154647e1087957830a8d31b
vadniks <whatever@fake.some>
2023-04-09 18:13:41 +0300

    Structure & logic

--- src/render.c
+++ src/render.c
@@ -4,15 +4,13 @@
 const unsigned ROWS = 4;
 const unsigned COLUMNS = ROWS;
 unsigned* gField = NULL;
+unsigned width = 0, height = 0;
 
 void initRenderer(SDL_Renderer* renderer) {
     gField = SDL_malloc(sizeof(unsigned) * ROWS * COLUMNS);
-
-    unsigned width = 0, height = 0;
     SDL_GetRendererOutputSize(renderer, (signed*) &width, (signed*) &height);
-    SDL_Log("%u %u\n", width, height);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 49, 54, 59, 255);
 }
 
 void doRender(SDL_Renderer* renderer) {
