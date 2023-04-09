c2bea6b77599f94a2166394b7f40e2d302e07c2e
vadniks <whatever@fake.some>
2023-04-09 21:03:25 +0300

    Structure & logic

--- src/render.c
+++ src/render.c
@@ -5,17 +5,18 @@
 const unsigned ROWS = 4, COLUMNS = ROWS, THICKNESS = 4;
 
 unsigned* gField = NULL;
-unsigned gWidth = 0, gHeight = 0, gTileSize = 0;
+unsigned gWidth = 0, gHeight = 0, gFieldSize = 0, gTileSize = 0;
 
 void setDrawColorToDefault(SDL_Renderer* renderer)
 { SDL_SetRenderDrawColor(renderer, 49, 54, 59, 255); }
 
 void initRenderer(SDL_Renderer* renderer) {
+    assert(ROWS == COLUMNS);
     gField = SDL_malloc(sizeof(unsigned) * ROWS * COLUMNS);
 
     SDL_GetRendererOutputSize(renderer, (signed*) &gWidth, (signed*) &gHeight);
-    assert(gWidth == gHeight && ROWS == COLUMNS);
-    gTileSize = (gWidth - (ROWS + 1) * THICKNESS) / ROWS;
+    gFieldSize = gWidth / 2;
+    gTileSize = (gFieldSize - (ROWS + 1) * THICKNESS) / ROWS;
 }
 
 void drawWindowFrame(SDL_Renderer* renderer) {
@@ -23,7 +24,7 @@ void drawWindowFrame(SDL_Renderer* renderer) {
     rect.x = 0;
     rect.y = 0;
     rect.w = (signed) (gWidth / THICKNESS);
-    rect.h = (signed) (gHeight / THICKNESS);
+    rect.h = (signed) (gWidth / THICKNESS);
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderSetScale(renderer, (float) THICKNESS, (float) THICKNESS);
@@ -36,12 +37,13 @@ void drawField(SDL_Renderer* renderer) {
 
     const int
         start = (signed) THICKNESS / 2,
-        end = (signed) (ROWS * gTileSize / THICKNESS + start);
+        end = (signed) (ROWS * gTileSize / THICKNESS + start),
+        margin = (signed) (gHeight / THICKNESS / 2 - gFieldSize / THICKNESS / 2);
 
     for (unsigned i = 0; i < ROWS + 1; i++) {
         int each = (signed) (i * gTileSize / THICKNESS + start);
-        SDL_RenderDrawLine(renderer, each, start, each, end);
-        SDL_RenderDrawLine(renderer, start, each, end, each);
+        SDL_RenderDrawLine(renderer, each, start + margin, each, end + margin);
+        SDL_RenderDrawLine(renderer, start, each + margin, end, each + margin);
     }
 }
 
