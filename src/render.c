af3c3da14e6a62dd0e3deefa966bb8f77fe84a1a
vadniks <whatever@fake.some>
2023-04-09 20:46:16 +0300

    Structure & logic

--- src/render.c
+++ src/render.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include "render.h"
 
-const unsigned ROWS = 4, COLUMNS = ROWS, THICKNESS = 5;
+const unsigned ROWS = 4, COLUMNS = ROWS, THICKNESS = 4;
 
 unsigned* gField = NULL;
 unsigned gWidth = 0, gHeight = 0, gTileSize = 0;
@@ -15,10 +15,10 @@ void initRenderer(SDL_Renderer* renderer) {
 
     SDL_GetRendererOutputSize(renderer, (signed*) &gWidth, (signed*) &gHeight);
     assert(gWidth == gHeight && ROWS == COLUMNS);
-    gTileSize = (gWidth - THICKNESS * 2) / ROWS / THICKNESS;
+    gTileSize = (gWidth - (ROWS + 1) * THICKNESS) / ROWS;
 }
 
-void drawWindowFrame(SDL_Renderer* renderer) { // TODO: deprecated
+void drawWindowFrame(SDL_Renderer* renderer) {
     SDL_Rect rect;
     rect.x = 0;
     rect.y = 0;
@@ -34,17 +34,24 @@ void drawField(SDL_Renderer* renderer) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderSetScale(renderer, (float) THICKNESS, (float) THICKNESS);
 
-    const int end = (signed) (gHeight / THICKNESS);
-    for (unsigned i = THICKNESS / 2; i < gWidth / THICKNESS; i += gTileSize)
-        SDL_RenderDrawLine(renderer, (signed) i, THICKNESS / 2, (signed) i, end - THICKNESS / 2),
-        SDL_RenderDrawLine(renderer, THICKNESS / 2, (signed) i, end - THICKNESS / 2, (signed) i); //TODO: clean up
+    const int end = (signed) (ROWS * gTileSize / THICKNESS + THICKNESS / 2);
+    for (unsigned i = 0; i < ROWS + 1; i++) {
+        int each = (signed) (i * gTileSize / THICKNESS + THICKNESS / 2);
+        SDL_RenderDrawLine(renderer, each, THICKNESS / 2, each, end);
+        SDL_RenderDrawLine(renderer, THICKNESS / 2, each, end, each);
+    }
+
+//    const int end = (signed) (gHeight / THICKNESS), start = 0/*(signed) THICKNESS / 2*/;
+//    for (unsigned i = start; i < gWidth / THICKNESS; i += gTileSize + THICKNESS)
+//        SDL_RenderDrawLine(renderer, (signed) i, start, (signed) i, end - start),
+//        SDL_RenderDrawLine(renderer, start, (signed) i, end - start, (signed) i);
 }
 
 void doRender(SDL_Renderer* renderer) {
     setDrawColorToDefault(renderer);
     SDL_RenderClear(renderer);
 
-//    drawWindowFrame(renderer);
+    drawWindowFrame(renderer);
     drawField(renderer);
 
     SDL_RenderPresent(renderer);
